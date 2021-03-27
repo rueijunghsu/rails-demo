3db27a892552b99ce761d7abeb58352139ecaec4
Ray Hsu <ray199185@gmail.com>
2021-03-27 14:33:24 +0800

    finished

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,11 +1,12 @@
 class CandidatesController < ApplicationController
   
+	before_action :find_candidate, only: [:show, :edit, :update, :destroy, :vote]
+
 	def index
 		@candidates = Candidate.all
 	end
 
 	def show
-		@candidate = Candidate.find_by(id: params[:id])
 	end
 
 	def new
@@ -16,30 +17,39 @@ class CandidatesController < ApplicationController
 		@candidate = Candidate.new(candidate_params)
 
 		if @candidate.save
-			flash[:notice] =  "Candidate Created !"
-			redirect_to '/candidates'
+			redirect_to '/candidates', notice: 'Candidate Created !'
 		else
 			render :new
 		end
 	end
 
 	def edit
-		@candidate = Candidate.find_by(id: params[:id])
 	end
 
 	def update
-		@candidate = Candidate.find_by(id: params[:id])
-
 		if @candidate.update(candidate_params)
-			flash[:notice] =  "Candidate Updated !"
-			redirect_to '/candidates'
+			redirect_to '/candidates', notice: 'Candidate Updated !'
 		else
 			render :edit
 		end
+	end
+
+	def destroy
+		@candidate.destroy
+		redirect_to '/candidates', notice: 'Candidate Deleted !'
+	end
+
+	def vote
+		@candidate.vote_logs.create(ip_address: request.remote_ip)
+		redirect_to '/candidates', notice: 'Voted !'
 	end
 
 	private
 	def candidate_params
 		params.require(:candidate).permit(:name, :party, :age, :politics)
 	end	
+
+	def find_candidate
+		@candidate = Candidate.find_by(id: params[:id])
+	end
 end	
